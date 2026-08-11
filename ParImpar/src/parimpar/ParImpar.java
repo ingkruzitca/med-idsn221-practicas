51f7fbaa9491456f52d6d4cf1c123fa3b7536aa4
ing cruz itca <gcruz@itca.edu.sv>
2026-08-10 18:54:55 -0600

    Agregando solucion para encontrar numeros pares e impares

--- ParImpar/src/parimpar/ParImpar.java
+++ ParImpar/src/parimpar/ParImpar.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package parimpar;
 
+import java.util.Scanner;
 /**
  *
  * @author Teacher Krux
@@ -15,6 +12,14 @@ public class ParImpar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int num = input.nextInt();
+        
+        if (num % 2 == 0) {
+            System.out.println(num +" es par.");
+        } else {
+            System.out.println(num +" es impar.");
+        }
+    }    
 }
